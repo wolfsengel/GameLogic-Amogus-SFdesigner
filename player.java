727950091b89bef6b5c4fc3238090f9730402292
angelgp <a22angelgp@iessanclemente.net>
2023-05-11 09:54:50 +0200

    Starting_Game

---
+++ player.java
@@ -0,0 +1,6 @@
+//clase abstracta de jugador
+public abstract class player {
+    //nombre, array de tareas, array de tareas completadas, booleano de vivo,
+String name;
+task[] tasks;
+}
---
+++ task.java
@@ -0,0 +1,4 @@
+public enum task {
+   //tareas del hogar
+   Limpiar_ventanas, recoger_la_habitación, limpiar_el_techo, limpiar_el_suelo, Operar_el_riñon, Baile_una_jota, Salto_en_barra, Juega_al_tenis, Juega_al_ping_pong, Juega_al_futbol, Juega_al_baloncesto, Juega_al_voleibol, Juega_al_beisbol, Juega_al_rugby, Juega_al_hockey, Juega_al_golf, Juega_al_billar, Juega_al_poker, Juega_al_blackjack, Juega_al_solitario, Juega_al_truco, Juega_al_tute, Juega_al_mus, Juega_al_chinchon, Juega_al_parchis, Juega_al_damas, Juega_al_ajedrez, Juega_al_estratego, Juega_al_risk, Juega_al_monopoly, Juega_al_cluedo, Juega_al_trivial, Juega_al_scrabble, Juega_al_ahorcado, Juega_al_3enraya, Juega_al_4enraya, Juega_al_7ymedio
+}
